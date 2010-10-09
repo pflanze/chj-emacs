5136d32fa473b0cfe2bdbe421e11a08cefae7de5
Christian Jaeger <chrjae@gmail.com>
2010-10-08 21:59:04 -0400

    keyboard fix for me, don't use M-s (but does that esc thing work hu?)

--- paredit.el
+++ paredit.el
@@ -416,7 +416,7 @@ Deprecated: use `paredit-mode' instead."
                 ("(foo |bar baz)"
                  "(foo (|bar) baz)"))
    ;;^ cool. warum dann unten nid ) ah weil ander taken wl. ich ding-.
-   ("M-s"       paredit-splice-sexp
+   ("ESC s"       paredit-splice-sexp
                 ("(foo (bar| baz) quux)"
                  "(foo bar| baz quux)"))
    (("M-<up>" "ESC <up>") ;; M-C-backspace aber da hats eh schon was brauchbares so..?
